152b6694ca6b92c28ad65633ea0fdcbaaad972e4
Ludvik Alkhoury <66024269+ludvikalkhoury@users.noreply.github.com>
2024-06-21 23:11:57 -0400

    more cleaning

--- Python/ARMBR/test_raw_mne.py
+++ Python/ARMBR/test_raw_mne.py
@@ -1,7 +1,7 @@
 from ARMBR_Library import *
 import scipy
 import mne
-
+import matplotlib.pyplot as plt
 
 raw = mne.io.read_raw_fif(r"..\SemiSyntheticData\Sub1\Sub1_Synthetic_Blink_Contaminated_EEG.fif", preload=True)
 raw.filter(l_freq=1, h_freq=40)
@@ -10,5 +10,8 @@ myARMBR = ARMBR()
 myARMBR.ImportFromRaw(raw)
 myARMBR.ARMBR(blink_chan=['C16','C29'])
 myARMBR.UnloadIntoRaw(raw)
+
+raw.plot()
+plt.show()
 
 
