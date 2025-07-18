964a9685336e4dee45bfdf584760cccf1f156325
Ludvik Alkhoury <66024269+ludvikalkhoury@users.noreply.github.com>
2025-07-18 11:42:44 -0400

    Modified the codes to work with Raw MNE. Core non-MNE-dependent code is also available `run_armbr`.

--- Python/ARMBR/mne_test_armbr.py
+++ Python/ARMBR/mne_test_armbr.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mne
 from mne.datasets import sample
-#from mne.preprocessing import ARMBR
-from armbr import ARMBR
+from armbr import ARMBR # or from ARMBR import ARMBR ; if you installed the ARMBR package
+
 
 def _load_mne_raw(with_annotation=True):
     # Load MNE sample EEG data
@@ -69,3 +69,22 @@ def test_armbr_raises_on_mixed_types():
     raw = _load_mne_raw()
     armbr = ARMBR(ch_name=["EEG 001", 2])
     armbr.fit(raw)
+    
+def test_armbr_plot_eeg_before_after_with_blinkspatialpattern():
+    # Load MNE sample EEG data
+    raw         = _load_mne_raw()
+    raw_before  = raw.copy()
+    raw_after   = raw.copy()
+
+    armbr = ARMBR(ch_name=["EEG 001"])
+    armbr.fit(raw_before, start=0, stop=20)
+    armbr.apply(raw_after)
+
+    # Plot raw before and after ARMBR
+    raw_before.plot(title="Before ARMBR", start=9, duration=4, n_channels=10, scalings='auto')
+    raw_after.plot(title="After ARMBR", start=9, duration=4, n_channels=10, scalings='auto')
+    
+    # Plot blink spatial pattern 
+    armbr.plot_blink_patterns()
+    
+    
